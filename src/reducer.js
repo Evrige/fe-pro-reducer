f99c7a1a40b20ae3da62fb1f728627a16234d024
Artur Dudnik <evrige.game@gmail.com>
2021-12-23 21:09:55 +0200

    ready reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,20 @@
 function reducer(input, action) {
-  // Ваше решение тут
+	switch (action) {
+		case "upperCase": input = input.toUpperCase();
+			break;
+		case "lowerCase": input = input.toLowerCase();
+			break;
+		case "capitalize": input = capitalize(input);
+			break;
+		default: input = "error";
+	}
+	return input;
 }
-
+function capitalize(string) {
+	let word = string.split(" ");
+	word = word.map(item => {
+		return item = (`${item[0].toUpperCase()}${item.slice(1)}`);
+	});
+	return string = word.join(" ");
+}
 module.exports = reducer;
